ae5a310d96dee892cd5113a21c02a5f39e979a9a
Kamil Langer <kamil.langer@caops.pl>
2023-11-14 21:28:38 +0100

    Fixed removing products from list

--- resources/js/Components/Repeater/ProductForm.vue
+++ resources/js/Components/Repeater/ProductForm.vue
@@ -4,15 +4,23 @@
         <input v-model="product.name" type="text" id="name">
         <label for="price">Price:</label>
         <input v-model="product.price" type="number" id="price">
-        X
+        <SecondaryButton @click="remove">X</SecondaryButton>
     </div>
 </template>
 
 <script>
+import SecondaryButton from "@/Components/SecondaryButton.vue";
+
 export default {
+    components: {SecondaryButton},
     props: {
         product: Object,
         index: Number
+    },
+    methods: {
+        remove() {
+            this.$emit('removeProduct', this.index);
+        }
     }
 };
 </script>
--- resources/js/Pages/Lists/Show.vue
+++ resources/js/Pages/Lists/Show.vue
@@ -10,6 +10,10 @@ const addProduct = () => {
     products.push({ name: '', price: null, shopName: null });
 };
 
+const removeProduct = (index) => {
+    products.splice(index, 1);
+};
+
 const submitForm = () => {
     var formData = new FormData();
 
@@ -49,6 +53,7 @@ export default {
                     :key="index"
                     :product="product"
                     :index="index"
+                    @removeProduct="removeProduct"
                 ></ProductForm>
                 <PrimaryButton type="button" @click="addProduct">Add Product</PrimaryButton>
                 <button type="submit">Submit</button>
@@ -56,57 +61,3 @@ export default {
         </div>
     </AuthenticatedLayout>
 </template>
-
-<!--<script setup>-->
-<!--import AuthenticatedLayout from '@/Layouts/AuthenticatedLayout.vue';-->
-<!--import {reactive, defineProps} from 'vue'-->
-<!--import {Head, router} from '@inertiajs/vue3'-->
-<!--import Select from "@/Components/Select.vue";-->
-<!--import PrimaryButton from "@/Components/PrimaryButton.vue";-->
-
-<!--const { list } = defineProps(['list']);-->
-
-<!--const id = list.id ?? null;-->
-
-<!--const form = reactive({-->
-<!--    name: list.name ?? null,-->
-<!--    description: list.description ?? null,-->
-<!--    status: list.status ?? null,-->
-<!--})-->
-
-<!--function submit() {-->
-<!--    if (id === null) {-->
-<!--        router.post('/lists', form)-->
-<!--    } else {-->
-<!--        router.put('/lists/' + list.id, form)-->
-<!--    }-->
-<!--}-->
-
-<!--const statuses = ['active', 'inactive', 'archive'];-->
-<!--</script>-->
-
-<!--<template>-->
-<!--    <Head title="Lists" />-->
-
-<!--    <AuthenticatedLayout>-->
-<!--        <template #header>-->
-<!--            <h2 class="font-semibold text-xl text-gray-800 leading-tight">Lists</h2>-->
-<!--        </template>-->
-
-<!--        <div class="py-4">-->
-<!--            <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">-->
-<!--                <div class="bg-white shadow-sm sm:rounded-lg">-->
-<!--                    <form @submit.prevent="submit" class="flex flex-col px-4 py-12">-->
-<!--                        <label for="name">List name:</label>-->
-<!--                        <input id="name" v-model="form.name" type="text" class=""/>-->
-<!--                        <label for="description">List description:</label>-->
-<!--                        <textarea id="description" v-model="form.description" />-->
-<!--                        <label for="status">List status:</label>-->
-<!--                        <Select :options="statuses" id="status" v-model="form.status"></Select>-->
-<!--                        <PrimaryButton type="submit" class="mt-4 justify-center">Save</PrimaryButton>-->
-<!--                    </form>-->
-<!--                </div>-->
-<!--            </div>-->
-<!--        </div>-->
-<!--    </AuthenticatedLayout>-->
-<!--</template>-->
